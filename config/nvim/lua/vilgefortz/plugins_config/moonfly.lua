84065dc2e5023048ecde4ae5ec0bbcb21c111923
Krak9n <alyoshagnatu@gmail.com>
2025-08-23 14:54:33 +0200

    updating dots

--- config/nvim/lua/vilgefortz/plugins_config/moonfly.lua
+++ config/nvim/lua/vilgefortz/plugins_config/moonfly.lua
@@ -1,4 +1,10 @@
-vim.o.termguicolors = true 
+vim.o.termguicolors = true
+vim.g.moonflyCursorColor = true
 
+
+--require("moonfly").custom_colors({
+  --  bg = "#121212",
+  -- violet = "#ff74b8",
+-- })
 vim.g.moonflyTransparent = true
 vim.cmd[[colorscheme moonfly]]
